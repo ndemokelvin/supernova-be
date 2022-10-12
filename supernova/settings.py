42b3a9f64c179078c3ecdbad233f03ad473e5b8d
kelvinndmo <ndemokelvinonkundi@gmail.com>
2022-10-12 12:10:52 +0300

    fix conflicts

--- supernova/settings.py
+++ supernova/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!j&f%wb87(3tzoner@lp-xv!liqp4*6n5)y)ly09m_8q!cb7nf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 AUTH_USER_MODEL = 'authentication.User'
